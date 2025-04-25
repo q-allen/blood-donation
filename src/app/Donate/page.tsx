bdc0da4b29468881a5aa7b616fe0863823904e16
Mary Rose Allen Y. Quijano <maryroseallenq@gmail.com>
2025-04-25 13:50:51 +0800

    centers

--- src/app/Donate/page.tsx
+++ src/app/Donate/page.tsx
@@ -36,7 +36,7 @@ export default function BloodDonationCenters() {
     const fetchHospitals = async () => {
       try {
         setIsLoading(true);
-        const response = await axios.get(`${apiUrl}/api/hospitals/hospital/`);
+        const response = await axios.get(`${apiUrl}/api/hospitals/`);
         setCenters(response.data);
       } catch (error) {
         console.error("Error fetching hospitals:", error);
